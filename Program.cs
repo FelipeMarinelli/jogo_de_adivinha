39a34e08175d08a50e87c52be26cef67129fa97d
FelipeMarinelli <98427995+FelipeMarinelli@users.noreply.github.com>
2024-07-28 20:19:02 -0300

    Add files via upload
    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo_de_Adivinha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool corect = false;
+            Random random = new Random();
+            int randomnumb = random.Next(1, 11);
+            Console.WriteLine("Vamos brincar de adivinha");
+            Console.WriteLine("eu VOU escolher um numero de 1 a 10");
+            Console.WriteLine("e voce vai tentar adivinhar!");
+
+            while (!corect)
+            {
+                Console.WriteLine("adivinhe");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                if (guess > randomnumb)
+                {
+                    Console.WriteLine("muito alto");
+                }
+                if (guess < randomnumb)
+                {
+                    Console.WriteLine("Baixo demais");
+                }
+                if (guess == randomnumb)
+                {
+                    Console.WriteLine("Parabens [:");
+                    corect = true;
+                }
+            }
+        }
+    }
+}
